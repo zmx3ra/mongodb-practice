fbb6bbc0f3bcb55822c61342dabb59d9c6aa9b3d
Neal Magee <nmagee@users.noreply.github.com>
2024-11-21 02:31:44 +0000

    Devcontainer builds

--- python/db.py
+++ python/db.py
@@ -4,7 +4,7 @@ import os
 
 
 MONGOPASS = os.getenv('MONGOPASS')
-uri = "mongodb+srv://cluster0.pnxzwgz.mongodb.net/sample_restaurants"
-client = MongoClient(uri, username='nmagee', password=MONGOPASS, connectTimeoutMS=200, retryWrites=True)
+uri = "mongodb+srv://cluster0.m3fek.mongodb.net/sample_restaurants"
+client = MongoClient(uri, username='ds2022', password=MONGOPASS, connectTimeoutMS=200, retryWrites=True)
 sampler = client.sample_restaurants
 restaurants = sampler.restaurants
