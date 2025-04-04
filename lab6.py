fe05a786202cc6b75804d8ca1f37d6043f397df8
Rachel Kersey <zmx3ra@virginia.edu>
2025-04-04 14:42:25 -0400

    Update lab6.py

--- lab6.py
+++ lab6.py
@@ -10,7 +10,7 @@ MPASS = os.getenv('MPASS')
 MHOST = os.getenv('MHOST')
 client = MongoClient(MHOST, username=MUSER, password=MPASS, connectTimeoutMS=200, retryWrites=True)
 sampler = client.sample_activities
-activities = sampler.activities
+activities = sampler.activities #all of this code is from the material that was attached to this lab
 
 
 new_record = {
@@ -52,10 +52,10 @@ activities.insert_one(new_record4)
 activities.delete_many({"name": "swimming"}) #needed to do this because swimming kept popping up (https://www.mongodb.com/docs/manual/tutorial/remove-documents/?msockid=28e5b96d2c1a6e6d0c2ca8d62dcc6f88)
 
 
-queries=activities.find().limit(3)
+queries=activities.find().limit(3) #assigning a variable with the find function with limit of 3 so the script can return to use three of our documents
 
 
-for i in queries:
+for i in queries: #https://www.w3schools.com/python/python_for_loops.asp (used the fruit example from this website, I tried to just print(queries) at first but that did not work
 	print(i)
 
 
